786abc17343e0ce7b4af91e49d0721796d6e0bd5
Delowar-maker <56905688+Delowar-maker@users.noreply.github.com>
2023-11-01 18:13:18 +0600

    d

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,16 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
+export async function getServerSideProps() {
+  const res = await fetch('https://api.github.com/repos/vercel/next.js')
+  const repo = await res.json()
+  return { props: { repo } }
+}
+ 
+export default function Page({ repo }) {
+  return repo.stargazers_count
+}
+
 
 export default function Home() {
   return (
